0021155597d4988e882100ae52b084d4b635b963
FelixS90 <felix.scheffler90@gmail.com>
2014-10-16 14:45:53 +0200

    a

--- worksheet1/ChipFelix/Heun.m
+++ worksheet1/ChipFelix/Heun.m
@@ -5,7 +5,7 @@ len = t_end/dt;
 
 y = [y0,zeros(1,len)];
 for n = 1:len
-    temp = Euler(f,y(n),dt,2*dt);
+    temp = Euler(f,y(n),dt,dt);
     y(n+1) = y(n) + dt * 0.5 * (f(y(n)) + f(temp(2)));
 end
 
--- worksheet1/Euler.m
+++ worksheet1/Euler.m
@@ -1,11 +1,11 @@
 function [ y ] = Euler( f,y0,dt,t_end )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-len = length(t_end/dt);
+len = t_end/dt;
 
-y = [y0,zeros(len)];
+y = [y0,zeros(1,len)];
 
-for n = 1:size(y,2)-1
+for n = 1:len
     y(n+1) = y(n) + dt*f(y(n));
 end
 
