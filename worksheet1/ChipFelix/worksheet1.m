57e13362b432b7db86a7fbd05c793efa75d171a6
waltsim <walter.a.simson@gmail.com>
2014-10-16 15:33:19 +0200

    RK tried new function version

--- worksheet1/ChipFelix/worksheet1.m
+++ worksheet1/ChipFelix/worksheet1.m
@@ -1,8 +1,8 @@
 %% Worksheet 1
 % a) Use matlab to plot the function p(t) in a graph.
 
-dt = 0.1;
-t_end = 9.9;
+dt = 1;
+t_end = 9;
 t = 0:dt:t_end;
 p = 10./(1+9.*exp(-t));
 
@@ -18,4 +18,10 @@ plot(t,y,'r-')
 %2)
 y = Heun(diff_func,y0,dt,t_end);
 plot(t,y,'g-')
+
+%3)
+
+y = RungeKutta(diff_func,y0,dt,t_end)
+
+plot(t,y,'b-')
 
