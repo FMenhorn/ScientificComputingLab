5b5da067cb774ced5ae2b1de404ef88e53b6b4e8
FelixS90 <felix.scheffler90@gmail.com>
2014-10-18 16:39:30 +0200

    Task e) completed (anyway still to be verified)
    Still to be clarified, why error decreases nearly twice as fast as when
    compared to analytical solution!

--- worksheet1/ChipFelix/worksheet1.m
+++ worksheet1/ChipFelix/worksheet1.m
@@ -11,7 +11,7 @@ xlabel('Time t')
 ylabel('p(t)')
 title('Analytical solution of p(t)')
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Program paused. Proceed with Task c). Press enter to continue.\n');
 pause;
 %% b)
 % see functions Euler.m, Heun.m, RungeKutta.m, (RKEuler.m)
@@ -92,9 +92,81 @@ legend(strcat('Runge-Kutta with dt =',' ',num2str(dt(1))),...
        strcat('Runge-Kutta with dt =',' ',num2str(dt(3))),...
        strcat('Runge-Kutta with dt =',' ',num2str(dt(4))),...
        'Analytical Solution', 'Location','northwest')
-title(['Comparison of Runge-Kutta approximations with respect to time step ' ...
-       'and analytical solution'])
+title(['Comparison of Runge-Kutta approximations with respect to ' ...
+       'time step and analytical solution'])
 hold off
+fprintf('Program paused. Proceed with Task e). Press enter to continue.\n');
+pause;
+
+%% d)
+% see tables in Worksheet 1
+
+%% e)
+
+eq1 = @(p)(1-(p/10))*p;
+y0 = 1;
+E_eul = zeros(1,length(dt));
+E_heu = zeros(1,length(dt));
+E_RK = zeros(1,length(dt));
+
+Color = {[1 1 0], [1 0 1], [0 1 1], [1 0 0], [0 1 0]};
+
+% Euler
+stepsize = 1;
+for i=length(dt):-1:1
+    ct = 1;
+    y = Euler(eq1,y0,dt(i),t_end);
+    if i == length(dt)
+        p = y;
+    end
+    
+    for j=1:stepsize:t_end/dt(length(dt))+1
+        diff_vec = p(j)-y(ct);
+        ct = ct + 1;
+    end
+    E_eul(i) = sqrt((dt(i)/5)*sum((diff_vec).^2));
+    stepsize = stepsize * 2;
+end
+E_eul
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
+% Heun
+stepsize = 1;
+for i=length(dt):-1:1
+    ct = 1;
+    y = Heun(eq1,y0,dt(i),t_end);
+    if i == length(dt)
+        p = y;
+    end
+    for j=1:stepsize:t_end/dt(length(dt))+1
+        diff_vec = p(j)-y(ct);
+        ct = ct + 1;
+    end
+    E_heu(i) = sqrt((dt(i)/5)*sum((diff_vec).^2));
+    stepsize = stepsize * 2;
+end
+E_heu
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+% Runge-Kutta
+stepsize = 1;
+for i=length(dt):-1:1
+    ct = 1;
+    y = RungeKutta(eq1,y0,dt(i),t_end);
+    if i == length(dt)
+        p = y;
+    end
+    for j=1:stepsize:t_end/dt(length(dt))+1
+        diff_vec = p(j)-y(ct);
+        ct = ct + 1;
+    end
+    E_RK(i) = sqrt((dt(i)/5)*sum((diff_vec).^2));
+    stepsize = stepsize * 2;
+end
+E_RK
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
