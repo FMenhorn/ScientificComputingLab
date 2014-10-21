d35caffa2bde8e81d1ebf5af3888b34f99d959e0
FMenhorn <friedi.menhorn@gmail.com>
2014-10-21 14:42:48 +0200

    added docu for worksheet1 script file

--- worksheet1/Erik_Friedrich/worksheet1.m
+++ worksheet1/Erik_Friedrich/worksheet1.m
@@ -1,7 +1,40 @@
 %Worksheet 1
+% In this first worksheet, the ode 
+%                   dp/dt = (1-p/10) * p (1) 
+% is examined. It describes the dynamics of the population of a 
+% certain species. The initial condition is given as 
+%                   p(0) = 1 (2)
+% and the analytical solution is
+%                   p(t) = 10/(1+9*exp(-t)) (3)
+% The purpose of this worksheet is to examine properties of different
+% numerical methods (namely Euler, Heun and Runge-Kutta) with the help 
+% of this ode.
+% The tasks are as follows:
+% a) plot function (3) in a graph
+% b) Implement the following methods for a general function dy/dt = f(y)
+%    for the initial condition y(0) = y_0:
+%       1) explicit Euler
+%       2) method of Heun
+%       3) Runge-Kutta method (fourth order)
+% c) Compute approximations for equation (1) with initial condition (2)
+%    end time t_end = 5 and time steps delta_t = 1,1/2,1/4,1/8. For each
+%    case compute the approximation error
+%                   error = sqrt(dt/5 sum_k (p_k - p_{k,exact})^2)) (4)
+%    where p_k represents the approximation and p_{k,exact} is the exact
+%    solution at t = delta_t * k;
+% d) For each error determine the factor by which the error is reduced if
+%    the step size delta_t is halved. (5)
+% e) Since in general the analytic solution is not know, compute the
+%    approximated error
+%                   error_approximated = 
+%                               sqrt(dt/5 sum_k (p_k - p_{k,best})^2)) (6)
+%    where again p_k denotes the approximation and p_{k,best} represents 
+%    the best solution computed with the smallest time step delta_t.
+% Show the result in tables at the end.
+format long
 
 %% variables for time discretization
-t_end = 9;
+t_end = 5;
 delta_t = 1;
 time_steps = 0:delta_t:t_end;
 %% 
@@ -19,7 +52,6 @@ diff_func = @(p) (1-p./10).*p;
 figure;
 analytic_time = 0:0.001:t_end;
 plot(analytic_time,analytical_sol(analytic_time),'black');
-%title('Analytical Solution');
 hold on;
 %%
 
@@ -29,21 +61,65 @@ euler_res = expl_euler(t_end,delta_t,y_0,diff_func);
 
 %figure;
 plot(time_steps,euler_res,'g');
-%title('Explicit Euler');
 
 % 2) Method of Heun
 heun_res = heun(t_end,delta_t,y_0,diff_func);
 
 %figure;
 plot(time_steps,heun_res,'b');
-%title('Method of Heun');
 
 % 3) Runge-Kutta
 runge_kutta_res = runge_kutta(t_end,delta_t,y_0,diff_func);
 
 %figure;
 plot(time_steps,runge_kutta_res,'r');
-%title('Runge-Kutta');
 legend('AnSol', 'ExplEul','Heun','RungeKutta','Location','southeast');
 title(['Comparison of ode solution methods for dt = ' num2str(delta_t)]);
+xlabel('time t in seconds');
+ylabel('y');   
+%%
+
+%% c)
+% variables for time discretization
+t_end = 5; %t_end stays the same
+delta_t = [1/8 1/4 1/2 1]; %delta_t is now an array
+delta_t = sort(delta_t); %to make sure that smallest delta_t is last
+length_dt = length(delta_t);
+
+% ODE settings (stay the same as before)
+% initial value
+y_0 = 1;
+
+% initiate arrays with similar structure to worksheet table
+% first row: different delta_t
+% second row: error (see (4))
+% third row: error reduced factor (see (5))
+% fourth row: error_approximated (see (6))
+% example:
+% euler_arr = nan(4,length_dt);
+% heun_arr = nan(4,length_dt);
+% runge_kutta_arr = nan(4,length_dt);
+
+% needed for output. Length of delta_t can be variable
+column_arr = [];
+for i = 1:length_dt
+    column_arr =[column_arr '_ '];
+end
+    
+% Explicit Euler
+func = @expl_euler;
+euler_arr = error_summation(func,diff_func,delta_t,t_end,y_0);
+printmat(euler_arr, 'Results of Euler-Method', 'delta_t absolute_error error_factor approx_error', column_arr)
+
+% Heun
+func = @heun;
+heun_arr = error_summation(func, diff_func, delta_t,t_end,y_0);
+printmat(heun_arr, 'Results of Heun-Method', 'delta_t absolute_error error_factor approx_error', column_arr)
+
+% Runge-Kutta
+func = @runge_kutta;
+runge_kutta_arr = error_summation(func, diff_func, delta_t, t_end, y_0);
+printmat(runge_kutta_arr, 'Results of Runge-Kutta-Method', 'delta_t absolute_error error_factor approx_error', column_arr)
+
+
     
