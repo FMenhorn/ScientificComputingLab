2d38a802e2062f8f3fb61dfc304affe13b4de4df
FMenhorn <friedi.menhorn@gmail.com>
2014-10-21 15:33:14 +0200

    added doc to analytical sol

--- worksheet1/Erik_Friedrich/analytical_sol.m
+++ worksheet1/Erik_Friedrich/analytical_sol.m
@@ -1,8 +1,11 @@
 function [ p ] = analytical_sol( t )
-%ANALYTICAL_SOL 
-% Calculates the analytical solution 
+%ANALYTICAL_SOL calculates the analytical solution 
 % of dp/dt = (1-p/10)*p, which is
 % p(t) = 10/(1+9 *e^(-t)).
+% INPUT: 
+%       t:          time t (can be a vector)
+% OUTPUT:
+%       p:          solution vector for all time steps of t
 
 p = 10./(1+9 .*exp(-t));
 
