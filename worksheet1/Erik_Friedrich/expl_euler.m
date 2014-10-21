2ef8a1fb83d7f244956deca17c33317e8a048efb
FMenhorn <friedi.menhorn@gmail.com>
2014-10-21 15:50:53 +0200

    added docu for expl_euler, heun and runge_kutta

--- worksheet1/Erik_Friedrich/expl_euler.m
+++ worksheet1/Erik_Friedrich/expl_euler.m
@@ -1,13 +1,21 @@
 function [ y_res ] = expl_euler( t_end,delta_t,y_0, diff_func )
-%EXPL_EULER Summary of this function goes here
-%   Detailed explanation goes here
+%EXPL_EULER solves the ode using the explicit Euler method
+% INPUT:
+%       t_end:  End time
+%       delta_t: time step size delta_t
+%       y_0:    Inital value at t=0
+%       diff_func: differential of y: dy/dt = diff_func(y)
+% OUTPUT:
+%       y_res: result vector for y values for each time step from 0:t_end
 
 % variables for time discretization
 time_steps = 0:delta_t:t_end;
 
+% initialize result vector and starting value
 y_res = nan(size(time_steps));
 y_res(1) = y_0;
 
+%explicit Euler
 for i = 1:(length(time_steps)-1)
     y_res(i+1) = y_res(i) + expl_euler_step(delta_t,diff_func(y_res(i)));
 end
