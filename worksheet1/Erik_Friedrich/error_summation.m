329d812568953c6a74af53eb82a4e4e27871e594
Erik <erik@erik-K53SV.(none)>
2014-10-21 14:55:56 +0200

    added doc on error_summation, changed function params

--- worksheet1/Erik_Friedrich/error_summation.m
+++ worksheet1/Erik_Friedrich/error_summation.m
@@ -1,31 +1,47 @@
-function [ result_arr ] = error_summation( func, diff_func, delta_t, t_end, y_0 )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ result_arr ] = error_summation( func, diff_func, analytic_sol_func, delta_t, t_end, y_0 )
+%ERROR_SUMMATION computes an array containing different error measures
+% INPUT: 
+%       func:         numerical solver function that returns a vector of function values for timesteps 0-t_end with spacing delta_t for the function dy/dt (y) = diff_func(y), starting at y=y_0 at t=0
+%                     (input: t_end, delta_t, y_0, diff_func, output: vector of y-values with length t_end/delta_t)
+%       diff_func:    function for evaluating the differential at y dy/dt (y) = diff_func(y)
+%       analytical_sol_func: exact solution to the ODE, y(t) = analytic_sol_func(t)
+%       delta_t:      array of timestep lengths used in the numerical integrations, for which different errors are calculated
+%       t_end:        calculation end time
+%       y_0:          initial value for y at t=0
+% OUTPUT:
+%       result_arr:   table with different error measurements, each column corresponding to the respective delta_t value
+%                     result_arr(1,:): value of delta_t in that column
+%                     result_arr(2,:): least-squares error compared with analytical soln
+%                     result_arr(3,i): factor of error size reduction compared with last timestep [=result_arr(2,i-1)/result_arr(2,i)]
+%                     result_arr(4,:): least-squares error compared with the solution using timestep size delta_t(1)
+%                       
+    %initialize variable and result
     length_dt = length(delta_t);
     result_arr = nan(4,length_dt);
     
-    for i = 1:length_dt
-        time_steps = 0:delta_t(i):t_end;
-        analytic_tmp = analytical_sol(time_steps);
-
+    %calculate reference function values 
+    time_steps = 0:delta_t(1):t_end;
+    result_analytic = analytic_sol_func(time_steps);
+    result_reference = func(t_end,delta_t(1),y_0,diff_func);
+    result_arr(2,1) = error_calc(delta_t(1), result_reference, result_analytic);
+    result_arr(4,1) = error_calc(delta_t(1), result_reference, result_reference);
+    
+    %loop over delta_t values
+    for i = 2:length_dt        
         result_tmp = func(t_end,delta_t(i),y_0,diff_func);
         result_arr(1,i) = delta_t(i);
-        result_arr(2,i) = error_calc(delta_t(i), result_tmp, analytic_tmp);
+  
+        delta_t_factor = delta_t(i)/delta_t(1); % assume factor is integer. Can correct otherwise. 
+                                                %TODO: assert this!
+        %extract reference elements
+        result_analytic_temp = result_analytic(1:delta_t_factor:end);
+        result_ref_tmp = result_reference(1:delta_t_factor:end);
+        
+        %compute errors
+        result_arr(2,i) = error_calc(delta_t(i), result_tmp, result_analytic_temp);
+        result_arr(4,i) = error_calc(delta_t(i), result_tmp, result_ref_tmp);
 
-        factor = delta_t(i)/delta_t(1);
-        if(i == 1)
-            result_best = result_tmp;
-        end
-        result_best_tmp = zeros(1,length(result_tmp));
-        k = 1;
-        for j = 1:factor:length(result_best)
-            result_best_tmp(k) = result_best(j);
-            k = k+1;
-        end
-        result_arr(4,i) = error_calc(delta_t(i), result_tmp, result_best_tmp);
-    end
-
-    for i = 2:length_dt
+        %compute error reduction factors
         result_arr(3,i) = result_arr(2,i)/result_arr(2,i-1);
     end
 
