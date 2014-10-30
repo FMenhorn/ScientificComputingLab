08dbfcc6fc6d5229fa1da1597bf232503690709f
waltsim <walter.a.simson@gmail.com>
2014-10-30 23:37:33 +0100

    testing

--- worksheet2/Chip_Felix/Euler.m
+++ worksheet2/Chip_Felix/Euler.m
@@ -1,4 +1,4 @@
-function [ y ] = Euler( f,y0,dt,t_end )
+function [ y , len ] = Euler( f,y0,dt,t_end )
 %EULER this function returns an euler approximation of a function
 %   this approximation of a function is based on the function f
 %   starting value y0 timesteps dt and the end time t_end
@@ -7,7 +7,7 @@ len = t_end/dt; %number of steps in range
 
 y = [y0,zeros(1,len)];
 
-for n = 1:len
+for n = 1 : len
     y(n+1) = y(n) + dt*f(y(n));
 end
 
--- worksheet2/Chip_Felix/Worksheet_2.m
+++ worksheet2/Chip_Felix/Worksheet_2.m
@@ -4,28 +4,31 @@ dif_p = @(p) 7*(1-p/10)*9;               % ODE
 
 p0 = 20;                                 % startvalue
 
-grow = @(t) 200./(20-10.*exp(-7.*t));    % Growth function
+grow = @(t) 200./(20-10*exp(-7.*t));    % Growth function
 
 %% a)
-N=5;
-
-t = linspace(N,0);
+t_end=5;
+p = 20;                                  % number of plotted points
+t = linspace(t_end,0,p)
 plot(t,grow(t));                         % Plotted p(t) in a graph
 
 %% b)
 
 dt = [1 1/2 1/4 1/8 1/16 1/32];
-eu_mat = zeros(length(dt));
-for i=1 : 1 %length(dt)
-   y = Euler(grow, p0,dt(i),N-1);
-   %TODO vectors must be the same length to plot
+eu_mat = zeros(t_end/dt(6));
+for i = 6 : 1 
+   eu_mat(:, i) = Euler(grow(), p0,dt(i),t_end);
+   %TODO vectors must be the same length to plot Note:  why doesn't this return
+   %the correct value matrix, what am i missing? the valaues arnt even
+   %writen into the matrix eu_mat
    %TODO fix for iteration so that a matrix is created with all Euler
    %values
    
+   
+   
 end
-length(y)  
-length(t)
 
-plot(t,y);
+% length(t) used to trouble shoot. returns 100. standard linspace value?
+plot(t / p,y);
 
 %TODO repeat the Euler iteration for Heun
