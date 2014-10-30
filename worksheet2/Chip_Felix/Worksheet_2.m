39b250b7b5d2f544025bde145bfeebce6b6094d9
waltsim <walter.a.simson@gmail.com>
2014-10-30 15:57:08 +0100

    Revisiona and added TODO's to WS2

--- worksheet2/Chip_Felix/Worksheet_2.m
+++ worksheet2/Chip_Felix/Worksheet_2.m
@@ -15,7 +15,17 @@ plot(t,grow(t));                         % Plotted p(t) in a graph
 %% b)
 
 dt = [1 1/2 1/4 1/8 1/16 1/32];
-eu_mat = zeros()
-for i=1 : len(dt)
-    Euler(grow, p0,dt(i),N)
+eu_mat = zeros(length(dt));
+for i=1 : 1 %length(dt)
+   y = Euler(grow, p0,dt(i),N-1);
+   %TODO vectors must be the same length to plot
+   %TODO fix for iteration so that a matrix is created with all Euler
+   %values
+   
 end
+length(y)  
+length(t)
+
+plot(t,y);
+
+%TODO repeat the Euler iteration for Heun
