b1e67601d1537cf96721fec03dc6e040845e5fe7
waltsim <walter.a.simson@gmail.com>
2014-11-13 21:54:55 +0100

    testing

--- worksheet3/Chip_Felix_Hasnain/worksheet3.m
+++ worksheet3/Chip_Felix_Hasnain/worksheet3.m
@@ -2,6 +2,8 @@
 %%Exercise a)
 
 %define N
+
+%Nx and Ny in this case are 4
 N=16;
 %define A
 clear all;
@@ -11,22 +13,24 @@ T = zeros(4); %init T vector
 x = zeros(16,1);
 ii=1; %define counter
 %define T
-for i=1:4
+for i=1:4 
     for j=1 :4
-        if not (j==1 | i==1 | j==4 | i==4)
+        %if not (j==1 | i==1 | j==4 | i==4)
             T(i,j) = 1;
-        end
+        %end
     end
 end
 
 %A(1,2) = 2; 
 
-%init x and A 
+%init x 
 for i=1:4
     for j=1 :4
-        
         x(ii)=T(i,j);
         ii = ii + 1;
-       %TODO define A
     end
 end
+
+   %TODO define A
+
+   
