0368a0e5ae8e227886c17ccfee54bbad9b1847c1
hasnain <hasnain.rehman@gmail.com>
2014-11-16 01:44:25 +0100

    Trying to do something

--- worksheet3/Chip_Felix_Hasnain/Worksheet3.m
+++ worksheet3/Chip_Felix_Hasnain/Worksheet3.m
@@ -12,45 +12,48 @@
 
 % see functions Agen.m, GaussSeidel.m, RHS.m
 
-%% Task d)
-
-Nx = 100;
-Ny = 100;
-hx = 1/(Nx + 1);
-hy = 1/(Ny + 1);
-
+%% Task d) and e)
 f = @(x,y)(-2*pi^2*sin(pi*x).*sin(pi*y));
+f_ana = @(x,y)(sin(pi*x).*sin(pi*y));
 
-% 1)
+Nx = [7 15 31 63];
+Ny = [7 15 31 63];
 
-A = Agen(Nx,Ny);
-b_temp = RHS(Nx,Ny,f);
-
-b = zeros(Nx*Ny,1);
-for i=1:Ny
-    b(Nx*(i-1)+1:Nx*(i-1)+Nx) = b_temp(:,i);
+% 1) MATLAB DIRECT SOLVER
+for n = 1 : length(Nx)
+	hx = 1/(Nx + 1);
+	hy = 1/(Ny + 1);
+	
+	A = Agen(Nx,Ny);
+% 	b_temp = RHS(Nx,Ny,f);
+	
+% 	b = zeros(Nx*Ny,1);
+% 	for i=1:Ny
+% 		b(Nx*(i-1)+1:Nx*(i-1)+Nx) = b_temp(:,i);
+% 	end
+	b = RHS(Nx,Ny,f);
+	b = b(:);
+	tic;
+	x = linsolve(A,b);
+	t1 = toc;
+	t1
+	
+	tic;
+	x = A\b;
+	t2 = toc;
+	t2
+	
+	% TODO: What is internally happening with \ solving opposed to linsolve?
+	%       \ is even faster!!! Why???
+	
+	X = zeros(Nx+2, Ny+2);
+	X(1:end)=x(1:end);
+	% for i=1:Ny
+	%     X(2:Nx+1,i+1) = x(Nx*(i-1)+1:Nx*(i-1)+Nx);
+	% end
+	
+	surf(X);
 end
-
-tic;
-x = linsolve(A,b);
-t1 = toc;
-t1
-
-tic;
-x = A\b;
-t2 = toc;
-t2
-
-% TODO: What is internally happening with \ solving opposed to linsolve?
-%       \ is even faster!!! Why???
-
-X = zeros(Nx+2, Ny+2);
-for i=1:Ny
-    X(2:Nx+1,i+1) = x(Nx*(i-1)+1:Nx*(i-1)+Nx);
-end
-
-surf(X);
-
 % 2)
 tic;
 
@@ -68,9 +71,10 @@ t3 = toc;
 t3
 
 X = zeros(Nx+2, Ny+2);
-for i=1:Ny
-    X(2:Nx+1,i+1) = x(Nx*(i-1)+1:Nx*(i-1)+Nx);
-end
+% for i=1:Ny
+%     X(2:Nx+1,i+1) = x(Nx*(i-1)+1:Nx*(i-1)+Nx);
+% end
+X(1:end)=x(1:end);
 
 surf(X);
 
