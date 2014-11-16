2464c17d92c153b5ef1193c5b06d6fc0ce0e5e0b
hasnain <hasnain.rehman@gmail.com>
2014-11-16 06:59:20 +0100

    Completed the remaining tasks.

--- worksheet3/Chip_Felix_Hasnain/error.m
+++
@@ -1,7 +0,0 @@
-function [ e ] = error ( Nx,Ny,A_GS,A_ana )
-%ERROR computes the error between the Gauss-Seidel implementation and the
-%analytical solution.
-%   Detailed explanation goes here
-e = sqrt(sum(sum((A_GS-A_ana).^2))/(Nx*Ny));
-end
-
