b8b95eafe67965e0bc6da3fa0783862efbbd167e
FelixS90 <felix.scheffler90@gmail.com>
2014-11-16 01:50:13 +0100

    Added error for-loop for task g) and error.m

--- worksheet3/Chip_Felix_Hasnain/Worksheet3.m
+++ worksheet3/Chip_Felix_Hasnain/Worksheet3.m
@@ -12,48 +12,45 @@
 
 % see functions Agen.m, GaussSeidel.m, RHS.m
 
-%% Task d) and e)
+%% Task d)
+
+Nx = 100;
+Ny = 100;
+hx = 1/(Nx + 1);
+hy = 1/(Ny + 1);
+
 f = @(x,y)(-2*pi^2*sin(pi*x).*sin(pi*y));
-f_ana = @(x,y)(sin(pi*x).*sin(pi*y));
 
-Nx = [7 15 31 63];
-Ny = [7 15 31 63];
+% 1)
 
-% 1) MATLAB DIRECT SOLVER
-for n = 1 : length(Nx)
-	hx = 1/(Nx + 1);
-	hy = 1/(Ny + 1);
-	
-	A = Agen(Nx,Ny);
-% 	b_temp = RHS(Nx,Ny,f);
-	
-% 	b = zeros(Nx*Ny,1);
-% 	for i=1:Ny
-% 		b(Nx*(i-1)+1:Nx*(i-1)+Nx) = b_temp(:,i);
-% 	end
-	b = RHS(Nx,Ny,f);
-	b = b(:);
-	tic;
-	x = linsolve(A,b);
-	t1 = toc;
-	t1
-	
-	tic;
-	x = A\b;
-	t2 = toc;
-	t2
-	
-	% TODO: What is internally happening with \ solving opposed to linsolve?
-	%       \ is even faster!!! Why???
-	
-	X = zeros(Nx+2, Ny+2);
-	X(1:end)=x(1:end);
-	% for i=1:Ny
-	%     X(2:Nx+1,i+1) = x(Nx*(i-1)+1:Nx*(i-1)+Nx);
-	% end
-	
-	surf(X);
+A = Agen(Nx,Ny);
+b_temp = RHS(Nx,Ny,f);
+
+b = zeros(Nx*Ny,1);
+for i=1:Ny
+    b(Nx*(i-1)+1:Nx*(i-1)+Nx) = b_temp(:,i);
 end
+
+tic;
+x = linsolve(A,b);
+t1 = toc;
+t1
+
+tic;
+x = A\b;
+t2 = toc;
+t2
+
+% TODO: What is internally happening with \ solving opposed to linsolve?
+%       \ is even faster!!! Why???
+
+X = zeros(Nx+2, Ny+2);
+for i=1:Ny
+    X(2:Nx+1,i+1) = x(Nx*(i-1)+1:Nx*(i-1)+Nx);
+end
+
+surf(X);
+
 % 2)
 tic;
 
@@ -71,10 +68,9 @@ t3 = toc;
 t3
 
 X = zeros(Nx+2, Ny+2);
-% for i=1:Ny
-%     X(2:Nx+1,i+1) = x(Nx*(i-1)+1:Nx*(i-1)+Nx);
-% end
-X(1:end)=x(1:end);
+for i=1:Ny
+    X(2:Nx+1,i+1) = x(Nx*(i-1)+1:Nx*(i-1)+Nx);
+end
 
 surf(X);
 
@@ -83,10 +79,19 @@ surf(X);
 
 % 3)
 
-tic;
-X = GaussSeidel(Nx,Ny,RHS(Nx,Ny,f));
-t4 = toc;
-t4
+Nx = [7 15 31 63 127];
+Ny = Nx;
+len = length(Nx);
+e = zeros(len,1);
+
+for i=1:len
+    tic;
+    X = GaussSeidel(Nx(i),Ny(i),RHS(Nx(i),Ny(i),f));
+    t4 = toc;
+    t4
+    e(i) = error(Nx(i),Ny(i),X,A_ana)
+end
+
 
 surf(X);
 
