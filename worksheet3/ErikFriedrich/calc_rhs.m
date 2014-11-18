bc747d9280acdd431c4bc1380a069500da120a9d
Erik <erik@erik-K53SV.(none)>
2014-11-18 12:45:49 +0100

    optimised code a bit, saved 1.8 secs or so on 63x63, still not very documented

--- worksheet3/ErikFriedrich/calc_rhs.m
+++ worksheet3/ErikFriedrich/calc_rhs.m
@@ -1,12 +1,12 @@
 function [ b ] = calc_rhs( N_x,N_y,func )
-%CALC_RHS calculates the righthandside of the PDE using the function func
-%   Detailed explanation goes here
+%CALC_RHS calculates the righthand vector of the PDE using the function func,
+%   on a rectangular grid on (0,1)², (without endpoints 0 and 1.)
 x = 0:1/(1+N_x):1;
 y = 0:1/(1+N_y):1;
 
 b = nan(N_x*N_y,1);
-for i = 1:N_x
-    for j = 1:N_y
+for j = 1:N_y
+    for i = 1:N_x
         b((j-1)*N_x+i) = func(x(i+1),y(j+1));
     end
 end
--- worksheet3/ErikFriedrich/gauss_seidel.m
+++ worksheet3/ErikFriedrich/gauss_seidel.m
@@ -4,17 +4,17 @@ function [ T_approx,frames] = gauss_seidel( b, N_x, N_y )
 
 h_x = 1/(N_x+1);
 h_y = 1/(N_y+1);
-N_x;
-N_y;
+% N_x;
+% N_y;
 x = 0:h_x:1;
 y = 0:h_y:1;
 hx_min2= 1/h_x^2;
 hy_min2= 1/h_y^2;
-doubneg_h_sum = -2*hx_min2-2*hy_min2;
+doubleneg_h_sum = -2*hx_min2-2*hy_min2;
 
-[X,Y] = meshgrid(x,y);
+% [X,Y] = meshgrid(x,y);
 
-T_approx = zeros(N_x+2,N_y+2);
+T_approx = zeros(N_y+2,N_x+2);
 accuracy_limit = 10^(-4);
 res = accuracy_limit+1;
 figure;
@@ -23,17 +23,22 @@ frames = [];
 counter=0;
 while ( res > accuracy_limit)
     res = 0;
-    for i = 2:N_x+1
-        for j = 2:N_y+1
-            left_value = T_approx(i,j-1)*hx_min2;
-            right_value = T_approx(i,j+1)*hx_min2;
-            top_value = T_approx(i+1,j)*hy_min2;
-            bottom_value = T_approx(i-1,j)*hy_min2;
-            T_approx(i,j) = (b((j-2)*N_x+i-1)-left_value-right_value-top_value-bottom_value)/doubneg_h_sum;
+    for i = 2:N_y+1
+%         top_values = T_approx(i+1,:);
+%         bottom_values = T_approx(i-1,:);
+%         right_values = [T_approx(i,2:(N_x+2)) 0];
+%         true_values = b((1:N_x)+(i-2)*N_x);
+%         
+%         T_tmp = ([0 true_values' 0] - (bottom_values + top_values)*hy_min2 - right_values*hx_min2);
+%        
+        T_tmp = ([0 b((1:N_x)+(i-2)*N_x)' 0] - (T_approx(i+1,:) + T_approx(i-1,:))*hy_min2 - [T_approx(i,2:(N_x+2)) 0]*hx_min2);
+        
+        for j = 2:N_x+1
+            T_approx(i,j) = (T_tmp(j)-T_approx(i,j-1)*hx_min2)/doubleneg_h_sum;
         end
     end
-    T_approx;
-    res_tmp = 0;
+%     T_approx;
+%     res_tmp = 0;
     %TODO RESIDUAL CALCULATION
     %HERE
     %NEW
@@ -42,23 +47,37 @@ while ( res > accuracy_limit)
 %         res_tmp = b(i) - 
 %     end
     %OLD
-    for i = 2:N_x+1
-        for j = 2:N_y+1
-            left_value = T_approx(i,j-1)*hx_min2;
-            right_value = T_approx(i,j+1)*hx_min2;
-            top_value = T_approx(i+1,j)*hy_min2;
-            bottom_value = T_approx(i-1,j)*hy_min2;
-            this_value = T_approx(i,j) *doubneg_h_sum;
-            res_tmp = res_tmp + (b((j-2)*N_x+i-1) - (left_value+right_value+top_value+bottom_value+this_value))^2;
-        end
-        %res = res + (b((j-2)*N_x+i-1) - res_tmp)^2;
-    end
+%     residual_left =   T_approx(2:end-1,1:end-2);
+%     residual_right =  T_approx(2:end-1,3:end  );
+%     residual_top =    T_approx(3:end  ,2:end-1);
+%     residual_bottom = T_approx(1:end-2,2:end-1);
+%     residual_matrix = (T_approx(2:end-1,2:end-1)*doubleneg_h_sum + (residual_left + residual_right)*hx_min2 + (residual_bottom + residual_top) * hy_min2)';
+    residual_matrix = (T_approx(2:end-1,2:end-1)*doubleneg_h_sum ...
+        + (T_approx(2:end-1,1:end-2) + T_approx(2:end-1,3:end  ))*hx_min2 ...
+        + (T_approx(3:end  ,2:end-1) + T_approx(1:end-2,2:end-1)) * hy_min2)';
+    
+%     residual_vector = b-residual_matrix(:);
+%     res= sqrt(1/(N_x*N_y))*norm(residual_vector);
+    res= sqrt(1/(N_x*N_y))*norm(b-residual_matrix(:));
+    
+%     for i = 2:N_x+1
+%         for j = 2:N_y+1
+%             left_value = T_approx(i,j-1)*hx_min2;
+%             right_value = T_approx(i,j+1)*hx_min2;
+%             top_value = T_approx(i+1,j)*hy_min2;
+%             bottom_value = T_approx(i-1,j)*hy_min2;
+%             this_value = T_approx(i,j) *doubleneg_h_sum;
+%             res_tmp = res_tmp + (b((j-2)*N_x+i-1) - (left_value+right_value+top_value+bottom_value+this_value))^2;
+%         end
+%         %res = res + (b((j-2)*N_x+i-1) - res_tmp)^2;
+%     end
         counter=counter+1;
-        res = sqrt(1/(N_x*N_y)*res_tmp);
+        %res = sqrt(1/(N_x*N_y)*res_tmp);
     if (mod(counter,100) == 0)
-        res_tmp
-        surf(X,Y,T_approx);
-        drawnow
+%         res_tmp
+        res
+        %surf(X,Y,T_approx);
+        %drawnow
     end
 end
 
