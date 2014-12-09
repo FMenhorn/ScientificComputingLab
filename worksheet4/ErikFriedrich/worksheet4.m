d66ca59abaa5f7ae0f9af1feaa1dab4b18148312
EWannerberg <daplff@gmail.com>
2014-12-09 11:17:29 +0100

    more abgefahrenes with the worksheet

--- worksheet4/ErikFriedrich/worksheet4.m
+++ worksheet4/ErikFriedrich/worksheet4.m
@@ -30,9 +30,9 @@ clear all;
 
 %% b) and c)
 t_end = [1/8 2/8 3/8 4/8];
-T_correct = nan(4,65,65);
-for cur_N_xy = 63
-    for cur_dt = 1/(2^14)
+T_correct = nan(4,129,129);
+for cur_N_xy = 127
+    for cur_dt = 1/(2^16)
         T_res = zeros(cur_N_xy+2,cur_N_xy+2);
         T_res(2:end-1,2:end-1) = ones(cur_N_xy,cur_N_xy);
         t_start = 0;
@@ -58,91 +58,91 @@ dt = [1/64 1/128 1/256 1/512 1/1024 1/2048 1/4096];
 N_xy = [3 7 15 31];
 error_val = nan(4,length(dt),length(N_xy));
 rel_error_val = nan(4,length(dt),length(N_xy));
-
-% Initialize the figures used for the subplots and initialize
-% subplot_counter which gives the position of the current subplot
-fig_array = [];
-% for t_end_index = 1:length(t_end)
-%     fig_array = [fig_array figure('name',['Explicit Euler with t_end ' num2str(t_end(t_end_index))],...
-%         'renderer','painters',...
-%         'units','normalized', ...
-%         'outerposition',[0 0 1 1], ...
-%         'PaperPositionMode','auto')];
-% %     suplabel(['(N_x,N_y)=' num2str(N_xy)],'y');
+% 
+% % Initialize the figures used for the subplots and initialize
+% % subplot_counter which gives the position of the current subplot
+% fig_array = [];
+% % for t_end_index = 1:length(t_end)
+% %     fig_array = [fig_array figure('name',['Explicit Euler with t_end ' num2str(t_end(t_end_index))],...
+% %         'renderer','painters',...
+% %         'units','normalized', ...
+% %         'outerposition',[0 0 1 1], ...
+% %         'PaperPositionMode','auto')];
+% % %     suplabel(['(N_x,N_y)=' num2str(N_xy)],'y');
+% % end
+% subplot_counter = 1;
+% % Start solving (1) using explicit Euler for different gridsizes N_xy
+% % (N_x=N_y), different dt and different t_end, receiving the result in
+% % T_res and plotting it in plotter.
+% % To save computation time, we do not start the computation always from
+% % zero, but from the previous t_end value, i.e. t_start(i) = t_end(i-1),
+% % since dt(i) = dt(i-1) and N_xy(i) = N_xy(i-1)
+% i_N = 1;
+% %i_dt = 1;
+h_ref = 1/128;
+% for cur_N_xy = N_xy
+%     i_dt = 1;
+%     for cur_dt = dt
+%         T_res = zeros(cur_N_xy+2,cur_N_xy+2);
+%         T_res(2:end-1,2:end-1) = ones(cur_N_xy,cur_N_xy);
+%         t_start = 0;
+%         for t_end_index = 1:length(t_end)
+%             %figure(fig_array(t_end_index))
+% 
+%             T_res = expl_euler(T_res,t_start,t_end(t_end_index),cur_dt,cur_N_xy,cur_N_xy);
+% 
+%             %handle = subplot(length(N_xy),length(dt),subplot_counter);
+%             %plotter(handle,cur_N_xy,T_res,['dt = 2^{' num2str(log2(cur_dt)) '}' ],length(dt),subplot_counter);
+% 
+%             t_start = t_end(t_end_index);
+%             
+%             h = 1/(cur_N_xy+1);
+%             h_ratio = h/h_ref;
+%             
+%             size(T_res(2:end-1,2:end-1));
+%             size(T_correct(t_end_index,2:h_ratio:(end-h_ratio-1),2:h_ratio:(end-h_ratio-1)));
+%             T_tmp = nan(cur_N_xy+2,cur_N_xy+2);
+%             T_tmp(:,:) = T_correct(t_end_index,1:h_ratio:end,1:h_ratio:end);
+%             size(T_tmp);
+%             error_matrix = T_res(:,:) - T_tmp(:,:);
+%             rel_error_matrix = error_matrix(2:end-1,2:end-1)./T_tmp(2:end-1,2:end-1);
+%             
+%             error_val(t_end_index,i_dt,i_N) = 1/cur_N_xy*norm(error_matrix(:));
+%             rel_error_val(t_end_index,i_dt,i_N) = 1/cur_N_xy*norm(rel_error_matrix(:));
+%         end
+%         subplot_counter = subplot_counter +1;
+%         i_dt = 1 + i_dt;
+%     end
+%     i_N = i_N +1;
 % end
-subplot_counter = 1;
-
-% Start solving (1) using explicit Euler for different gridsizes N_xy
-% (N_x=N_y), different dt and different t_end, receiving the result in
-% T_res and plotting it in plotter.
-% To save computation time, we do not start the computation always from
-% zero, but from the previous t_end value, i.e. t_start(i) = t_end(i-1),
-% since dt(i) = dt(i-1) and N_xy(i) = N_xy(i-1)
-i_N = 1;
-%i_dt = 1;
-h_ref = 1/64;
-for cur_N_xy = N_xy
-    i_dt = 1;
-    for cur_dt = dt
-        T_res = zeros(cur_N_xy+2,cur_N_xy+2);
-        T_res(2:end-1,2:end-1) = ones(cur_N_xy,cur_N_xy);
-        t_start = 0;
-        for t_end_index = 1:length(t_end)
-            %figure(fig_array(t_end_index))
-
-            T_res = expl_euler(T_res,t_start,t_end(t_end_index),cur_dt,cur_N_xy,cur_N_xy);
-
-            %handle = subplot(length(N_xy),length(dt),subplot_counter);
-            %plotter(handle,cur_N_xy,T_res,['dt = 2^{' num2str(log2(cur_dt)) '}' ],length(dt),subplot_counter);
-
-            t_start = t_end(t_end_index);
-            
-            h = 1/(cur_N_xy+1);
-            h_ratio = h/h_ref;
-            
-            size(T_res(2:end-1,2:end-1))
-            size(T_correct(t_end_index,2:h_ratio:(end-h_ratio-1),2:h_ratio:(end-h_ratio-1)))
-            T_tmp = nan(cur_N_xy+2,cur_N_xy+2);
-            T_tmp(:,:) = T_correct(t_end_index,1:h_ratio:end,1:h_ratio:end);
-            size(T_tmp)
-            error_matrix = T_res(:,:) - T_tmp(:,:);
-            rel_error_matrix = error_matrix(2:end-1,2:end-1)./T_tmp(2:end-1,2:end-1);
-            
-            error_val(t_end_index,i_dt,i_N) = 1/cur_N_xy*norm(error_matrix(:));
-            rel_error_val(t_end_index,i_dt,i_N) = 1/cur_N_xy*norm(rel_error_matrix(:));
-        end
-        subplot_counter = subplot_counter +1;
-        i_dt = 1 + i_dt;
-    end
-    i_N = i_N +1;
-end
-column_label =[];
-for i = 1:16
-    column_label = [column_label '(' num2str(i) ',' num2str(i) ') '];
-end
-tmp = nan(length(dt),length(N_xy));
-for i = 1:4
-tmp(:,:) = error_val(i,:,:).*(error_val(i,:,:)<1.0);
-tmp
-
-%printmat(tmp,'error',column_label,column_label);
-end
-for i = 1:4
-rel_tmp(:,:) = rel_error_val(i,:,:).*(rel_error_val(i,:,:)<1.0);
-rel_tmp
-
-%printmat(tmp,'error',column_label,column_label);
-end
-return
-%fig_labeler(fig_array, t_end);
-%%
-
+% column_label =[];
+% for i = 1:16
+%     column_label = [column_label '(' num2str(i) ',' num2str(i) ') '];
+% end
+% tmp = nan(length(dt),length(N_xy));
+% for i = 1:4
+% tmp(:,:) = error_val(i,:,:).*(error_val(i,:,:)<1.0);
+% tmp
+% 
+% %printmat(tmp,'error',column_label,column_label);
+% end
+% for i = 1:4
+% rel_tmp(:,:) = rel_error_val(i,:,:).*(rel_error_val(i,:,:)<1.0);
+% rel_tmp
+% 
+% %printmat(tmp,'error',column_label,column_label);
+% end
+% %fig_labeler(fig_array, t_end);
+% %%
 %% d) and e)
 
 % Starting parameters as given in worksheet
 t_end = [1/8 2/8 3/8 4/8];
 dt = [1/64];
 N_xy = [3 7 15 31];
+
+error_val = nan(4,length(dt),length(N_xy));
+rel_error_val = nan(4,length(dt),length(N_xy));
 
 % Initialize the figures used for the subplots and initialize
 % subplot_counter which gives the position of the current subplot
@@ -163,7 +163,10 @@ accuracy_limit = 1e-6;
 % Again to save computation time, we do not start the computation always from
 % zero, but from the previous t_end value, i.e. t_start(i) = t_end(i-1),
 % since dt(i) = dt(i-1) and N_xy(i) = N_xy(i-1)
+i_N = 1;
 for cur_N_xy = N_xy
+    
+    i_dt = 1;
     for cur_dt = dt
         T_res = zeros(cur_N_xy+2,cur_N_xy+2);
         T_res(2:end-1,2:end-1) = ones(cur_N_xy,cur_N_xy);
@@ -177,9 +180,37 @@ for cur_N_xy = N_xy
             
             t_start = t_end(t_end_index);
             subplot_counter = subplot_counter +1;
+            
+            h = 1/(cur_N_xy+1);
+            h_ratio = h/h_ref;
+            
+            T_tmp = nan(cur_N_xy+2,cur_N_xy+2);
+            T_tmp(:,:) = T_correct(t_end_index,1:h_ratio:end,1:h_ratio:end);
+            error_matrix = T_res(:,:) - T_tmp(:,:);
+            rel_error_matrix = error_matrix(2:end-1,2:end-1)./T_tmp(2:end-1,2:end-1);
+            
+            error_val(t_end_index,i_dt,i_N) = 1/cur_N_xy*norm(error_matrix(:));
+            rel_error_val(t_end_index,i_dt,i_N) = 1/cur_N_xy*norm(rel_error_matrix(:));
         end
+        i_dt = i_dt + 1;
     end
     
+    i_N = i_N + 1;
 end
 %suplabel('Implicit Euler with dt = 1/64','t');
-%saveas(fig_array,'plots/ImplicitEuler.png');
+saveas(fig_array,'plots/ImplicitEuler.png');
+tmp = nan(length(dt),length(N_xy));
+for i = 1:4
+tmp(:,:) = error_val(i,:,:).*(error_val(i,:,:)<1.0);
+tmp
+
+%printmat(tmp,'error',column_label,column_label);
+end
+for i = 1:4
+rel_tmp(:,:) = rel_error_val(i,:,:).*(rel_error_val(i,:,:)<1.0);
+rel_tmp
+
+%printmat(tmp,'error',column_label,column_label);
+end
+%fig_labeler(fig_array, t_end);
+%%
